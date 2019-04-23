aa6cd7b39f52806fb20a49f84125cc1a6cdb3c7a
matt ruggiero <matt@mattruggiero.com>
2019-04-22 20:55:22 -0700

    browseAll route returns all records in collection

--- server/routes/shop/browseAll.js
+++ server/routes/shop/browseAll.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoDB = require('../../config/keys').mongoURI;
 
-
+const Record =require('../../models/Record');
 
 router.get('/',(req,res)=>{
-    res.send("BROWSE ALL WORKS");
+    Record.find()
+        .then(records => {
+            res.send(records);
+        })
+    
 })
 
 
