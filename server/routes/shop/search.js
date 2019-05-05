c739e9af2f93f5de5bfa86897d480575f80dae30
Matt Ruggiero <matt@mattruggiero.com>
2019-05-04 22:42:56 -0700

    update

--- server/routes/shop/search.js
+++ server/routes/shop/search.js
@@ -10,7 +10,8 @@ router.route('*').all((req,res,next)=>{
     let searchFor = req.body.searchInput;
     console.log(searchFor);
     console.log('search worked');
-    let results = await Record.find({artist:searchFor}).lean();
+    //let results = await Record.find({artist:searchFor}).lean();
+    let results = await Record.find({$text:{$search:searchFor}}).lean()
     console.log(results.length);
     res.send(results);
 })
