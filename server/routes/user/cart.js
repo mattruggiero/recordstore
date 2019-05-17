e0d9d153eb9ca8823fb0bf5ad8b0e9878553e404
matt ruggiero <matt@mattruggiero.com>
2019-05-17 13:35:02 -0700

    cart route

--- server/routes/user/cart.js
+++ server/routes/user/cart.js
@@ -4,28 +4,69 @@ const User = require('../../models/User');
 const Record = require('../../models/Record');
 const jwtDecode = require('jwt-decode');
 
-router.post('/',async (req,res) =>{
-    let dirtyToken = req.headers.authorization;
-    let cleanToken = dirtyToken.replace(/Bearer/g,'').trim;
-    let decodedToken = jwtDecode(cleanToken);
 
-    let userDBID = decodedToken.id;
-    let recordDBID = req.body.recordDBID;
-    
+async function decodeToken(jwtToken){
+    try{
+        let dirtyToken = jwtToken;
+        let cleanToken = dirtyToken.replace(/Bearer/g,'').trim();
+        let decodedToken = jwtDecode(cleanToken);
+        return decodedToken;
+    }
+    catch(error){console.log(error);}
+}
+//get/return cart no changes 
+router.post('/', async (req,res)=>{
+    try{
+        let decodedToken = await decodeToken(req.headers.authorization);
+        let userEmail = decodedToken.email;
 
-    //let user = await User.findOne({email:decodedToken.email}).lean();
-    let user = await User.findById(decodedToken.id);
-    console.log(user);
-    let record = await Record.findById(recordDBID);
-    //console.log(record);
-    //console.log(user);
-    
-    
+        let user = await User.findOne({email:userEmail});
+        console.log('sending back cart');
+        res.send(user.cart);
+    }
+    catch(error){console.log(error);}
 })
+//add record to cart
+.post('/add',  async (req,res) =>{
+    try{
+        let decodedToken = await decodeToken(req.headers.authorization);
+        let userEmail = decodedToken.email;
+        let recordDBID = req.body.recordDBID;
+        let record = await Record.findById(recordDBID);
+        let user = await User.findOneAndUpdate({email:userEmail},{$push: {cart:record}})
 
+        console.log("added to cart  ", user.cart.length);
+        //should return cart and store in state here
+        res.send('done');
+    }
+    catch(error){console.log(error);}
+})
+//remove record to cart
+.post('/remove', async (req,res) => {
+    try{
+        let decodedToken = await decodeToken(req.headers.authorization);
+        let userEmail = decodedToken.email;
+        let recordDBID = req.body.recordDBID;
 
+        let record = await Record.findById(recordDBID);
+        let user = await User.findOneAndUpdate({email:userEmail}, {$pullAll:{cart:[record]}});
 
+        console.log('removed from cart: ', user.cart.length);
+        res.send('done');
+    }
+    catch(error){console.log(error);}
+})
+//empty cart 
+.post('/empty', async (req,res) =>{
+    try{
+        let decodedToken = await decodeToken(req.headers.authorization);
+        let userEmail = decodedToken.email;
+        let user = await User.findOneAndUpdate({email:userEmail},{$set:{cart:[]}});
 
-
+        console.log("emptied cart:  ",user.cart.length);
+        res.send('done');
+    }
+    catch(error){console.log(error);}
+})
 
 module.exports = router;
